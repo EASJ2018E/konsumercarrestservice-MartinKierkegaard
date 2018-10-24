b52c46c0383ff62c1d19fea9c028db7f2ed61849
Martin Kierkegaard <mark@easj.dk>
2018-10-24 11:25:45 +0200

    tilføjet .catch til get axios kald

--- src/js/index.ts
+++ src/js/index.ts
@@ -10,25 +10,30 @@ interface ICar {
     price:number;
 }
 
+let divElement : HTMLDivElement = <HTMLDivElement> document.getElementById("content");
 let buttonelement:HTMLButtonElement = <HTMLButtonElement> document.getElementById("getAllButton");  
 buttonelement.addEventListener('click',showAllCars);
 
 function showAllCars():void {
 
-    let uri :string = "http://rest-pele-easj-dk.azurewebsites.net/api/Cars";
+    let uri :string = "http://rest-pele-easj-dk.azurewebsites.net/api3/Cars";
 
     axios.get<ICar[]>(uri)
     .then(function (response:AxiosResponse<ICar[]>):void{
 
         let result : string = "<ol>";
         response.data.forEach((car : ICar) => {
-            result += "<li>"+ car.model + car.vendor + car.price.toString() +"</li>"
+            result += "<li> <b>model</b>: "+ car.model + " <i>vendor</i> :" + car.vendor +" pris:" +car.price.toString() +"</li>"
         });
 
         result += "</ol>";
 
+        divElement.innerHTML = result;
+
     }
     )
-    .catch()
+    .catch(function (error:AxiosError):void{
+            divElement.innerHTML= error.message;        
+    })
 
 }
